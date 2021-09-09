8601b553ca7a3d6461f65c55b42245d4f3b8235d
sarad-singh <sarad.singh@introcept.co>
2021-09-09 10:24:50 +0545

    add logger

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,17 @@
 import express, { Request, Response } from 'express'
+import morgan from 'morgan';
 import { config } from './config/config'
 import { employeeRouter } from './routes/employee';
 
 export const app = express()
 app.set('view engine', 'ejs');
 
+app.use(morgan('tiny'))
+app.use(express.urlencoded({ extended: false }))
+app.use('/employee', employeeRouter)
 app.get('/', (req: Request, res: Response) => {
     res.render('index')
 })
-
-app.use('/employee', employeeRouter)
 app.listen(config.port, () => {
     console.log(`App started on port: ${config.port}`)
 })
