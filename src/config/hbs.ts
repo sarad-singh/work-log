9474080c129774510d39bc22d857b87a0350d831
sarad-singh <sarad.singh@introcept.co>
2021-09-14 09:09:23 +0545

    fix registerHbsHelper:toLocaleDateString return

--- src/config/hbs.ts
+++ src/config/hbs.ts
@@ -2,6 +2,6 @@ import hbs from 'hbs'
 
 export const registerHbsHelpers = () => {
     hbs.registerHelper('toLocaleDateString', (dateString: string) => {
-
+        return (new Date(dateString)).toLocaleString()
     });
 }
