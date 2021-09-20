cda06b63c1798035d13b239832d38ea4395bee00
sarad-singh <sarad.singh@introcept.co>
2021-09-20 12:05:11 +0545

    register capitalize method for hbs

--- src/config/hbs.ts
+++ src/config/hbs.ts
@@ -3,5 +3,15 @@ import hbs from 'hbs'
 export const registerHbsHelpers = (): void => {
     hbs.registerHelper('toLocaleDateString', (dateString: string): string => {
         return (new Date(dateString)).toLocaleString()
+    })
+    hbs.registerHelper('capitalize', (string: string): string => {
+        const words = string.split(" ")
+        const capitalized: string[] = []
+        words.forEach(word => {
+            word = word[0].toUpperCase() + word.slice(1)
+            capitalized.push(word)
+        })
+        console.log(capitalized.join(" "))
+        return capitalized.join(" ")
     });
 }
